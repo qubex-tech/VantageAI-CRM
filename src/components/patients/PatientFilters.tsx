d0b1960fa39f50a1c5d41a953dfb8ebdcfcde460
saqib-qubex <64747887+saqib-qubex@users.noreply.github.com>
2025-12-31 11:00:26 -0600

    Reduce spacing in filter tabs container
    - Reduce TabsList padding from p-1 to p-0.5
    - Add gap-0.5 to reduce space between tabs
    - Reduce container padding in PatientFilters from p-1 to p-0.5
    - This makes the tabs much tighter with minimal white space

--- src/components/patients/PatientFilters.tsx
+++ src/components/patients/PatientFilters.tsx
@@ -200,7 +200,7 @@ export function PatientFilters({
             }
           }
         }} className="flex-1">
-          <TabsList className="w-full justify-start h-auto p-1 bg-gray-100">
+          <TabsList className="w-full justify-start h-auto p-0.5 bg-gray-100 gap-0.5">
             <TabsTrigger value="default" className="data-[state=active]:bg-white">
               All Patients
             </TabsTrigger>
--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -12,7 +12,7 @@ const TabsList = React.forwardRef<
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "inline-flex h-10 items-center justify-center rounded-lg bg-gray-100 p-1 text-gray-500",
+      "inline-flex h-auto items-center justify-center rounded-lg bg-gray-100 p-0.5 text-gray-500 gap-0.5",
       className
     )}
     {...props}
